f448b377d4f680675188de5ee640fdb5b476636f
davalarcon <zilentstorm@gmail.com>
2017-07-12 09:50:16 -0400

    import-export

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+//app.ts
+var math_1 = require("./math");
+var math_2 = require("./math");
+// we could also write:
+// import {powerOf2, randomInteger} from './math';
+//SAME THING! :)
+console.log(math_1.powerOf2(50));
+console.log(math_2.randomInteger(101));
---
+++ app.ts
@@ -0,0 +1,12 @@
+//app.ts
+import {powerOf2} from './math';
+
+import {randomInteger} from './math'
+
+// we could also write:
+// import {powerOf2, randomInteger} from './math';
+//SAME THING! :)
+
+console.log(powerOf2(50));
+
+console.log(randomInteger(101));
